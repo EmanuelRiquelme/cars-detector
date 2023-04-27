80e3b5557bc8403a3502effcab59029a98af0a88
EmanuelRiquelme <112744690+EmanuelRiquelme@users.noreply.github.com>
2023-04-26 22:26:09 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import torch
 
 def save_model(model,optimizer):
     checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
-    torch.save(checkpoint, 'dog_breed.pth.tar')
+    torch.save(checkpoint, 'cars.pth.tar')
 
-def load_model(model,optimizer,file_name='dog_breed.pth.tar'):
+def load_model(model,optimizer,file_name='cars.pth.tar'):
     checkpoint = torch.load(file_name)
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
